87f3b14270074337d049d5c26e0107fc7b7466ca
Joan Andrés <andres6936@live.com>
2017-10-21 19:48:17 -0500

    Modify PanelExtension.java

--- src/edu/jabs/bancoGnomos/interfaz/PanelExtension.java
+++ src/edu/jabs/bancoGnomos/interfaz/PanelExtension.java
@@ -1,26 +1,112 @@
 package edu.jabs.bancoGnomos.interfaz;
 
-public class PanelExtension
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.border.TitledBorder;
+
+/**
+ * Panel para el manejo de extensiones.
+ */
+public class PanelExtension extends JPanel implements ActionListener
 {
 
-    //-----------------------------------
+    // -----------------------------------------------------------------
     // Constantes
-    //-----------------------------------
+    // -----------------------------------------------------------------
 
-    //-----------------------------------
+    /**
+     * Comando Opción 1.
+     */
+    private static final String OPCION_1 = "OPCION_1";
+
+    /**
+     * Comando Opción 2.
+     */
+    private static final String OPCION_2 = "OPCION_2";
+
+    // -----------------------------------------------------------------
     // Atributos
-    //-----------------------------------
+    // -----------------------------------------------------------------
 
-    //-----------------------------------
-    // Constructor
-    //-----------------------------------
+    /**
+     * Ventana principal de la aplicación.
+     */
+    private InterfazBancoGnomos principal;
 
-    public PanelExtension()
+    // -----------------------------------------------------------------
+    // Atributos de interfaz
+    // -----------------------------------------------------------------
+
+    /**
+     * Botón opción 1.
+     */
+    private JButton btnOpcion1;
+
+    /**
+     * Botón opción 2.
+     */
+    private JButton btnOpcion2;
+
+    // -----------------------------------------------------------------
+    // Constructores
+    // -----------------------------------------------------------------
+
+    /**
+     * Constructor del panel.
+     * @param pPrincipal Ventana principal. ventana!=null.
+     */
+    public PanelExtension( InterfazBancoGnomos pPrincipal )
     {
+        principal = pPrincipal;
+
+        setBorder( new TitledBorder( "Opciones" ) );
+        setLayout( new GridBagLayout( ) );
+
+        // Botón opción 1
+        btnOpcion1 = new JButton( "Opción 1" );
+        btnOpcion1.setActionCommand( OPCION_1 );
+        btnOpcion1.addActionListener( this );
+        GridBagConstraints gbc = new GridBagConstraints( );
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.insets = new Insets( 0, 0, 0, 10 );
+        add( btnOpcion1, gbc );
+
+        // Botón opción 2
+        btnOpcion2 = new JButton( "Opción 2" );
+        btnOpcion2.setActionCommand( OPCION_2 );
+        btnOpcion2.addActionListener( this );
+        gbc = new GridBagConstraints( );
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        add( btnOpcion2, gbc );
     }
 
-    //-----------------------------------
+    // -----------------------------------------------------------------
     // Métodos
-    //-----------------------------------
+    // -----------------------------------------------------------------
+
+    /**
+     * Manejo de los eventos de los botones.
+     * @param e Acción que generó el evento.
+     */
+    public void actionPerformed( ActionEvent e )
+    {
+        if( OPCION_1.equals( e.getActionCommand( ) ) )
+        {
+            principal.reqFuncOpcion1( );
+        }
+        else if( OPCION_2.equals( e.getActionCommand( ) ) )
+        {
+            principal.reqFuncOpcion2( );
+        }
+    }
 
 }
